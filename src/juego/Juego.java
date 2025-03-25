2534690ea9d93eac2439c0a289b380374363fdc0
JMC-1DAW <javier.martin.1daw@gmail.com>
2025-03-25 18:39:17 +0100

    The wrong f-cking letter update
    I added the z0 and z1 always block thing but it makes me mad that it uses z instead of y for height, help-

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -46,6 +46,9 @@ public class Juego {
 		//Creamos el jugador
 		Jugador yo = new Jugador("Jorge");
 
+		//TODO Crear un menú que sustituya el bloque de abajo por lo que pide el nuevo pdf
+		
+		
 		//El Jugador recorre el mapa entero recolectando materias primas
 		for (int i= 0; i <TAMANO_MUNDO; i++) {
 			for (int j= 0; j <TAMANO_MUNDO; j++) {
@@ -77,8 +80,19 @@ public class Juego {
 
 		//Ponemos el numero de materias +2, se sale del rango (default)
 		//para que los casos +1 y +2 que no estan contemplados, generen bloques vacios
-		int tipo = rd.nextInt(Bloque.NUM_MATERIAS+2);
+		
+		// Ya que se estableció la regla de que los bloques por debajo de z = 1
+		// no pueden ser vacíos, separamos el aleatorio en 2 y elegimos el que
+		// encaje mejor, si z es 1 o 0 no podrá generar un bloque vacío y si
+		// z es mayor funcionará igual que antes
+		int tipo;
 
+		if (z <= 1) {
+			tipo = rd.nextInt(Bloque.NUM_MATERIAS);
+		} else {
+			tipo = rd.nextInt(Bloque.NUM_MATERIAS+2);
+		}
+		
 		switch (tipo) {
 		case Bloque.ALBERO: {
 			bloque = new BloqueAlbero(x, y, z);
